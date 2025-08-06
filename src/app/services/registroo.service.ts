c47633c0a51707576c3c99c9bbc17eeb58d63f92
Nosaka King <raulduran2808@gmail.com>
2025-08-06 10:06:13 -0500

    feat: Register Service API URL

--- src/app/services/registroo.service.ts
+++ src/app/services/registroo.service.ts
@@ -9,11 +9,13 @@ import { environment } from 'src/environments/environment';
 })
 export class RegistroAppService {
    private apiUrl = environment.apiUrl;
+   private registroUrl = `${this.apiUrl}/users/register`;
+
 
   constructor(private http: HttpClient) { }
 
   post(request: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, request)
+    return this.http.post<any>(this.registroUrl, request)
     .pipe()
   }
  
